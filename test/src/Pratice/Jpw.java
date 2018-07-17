beffeaf4af984d2f20cd64d61a3bb82cf9fc8f37
koitt03-A <koitt03-A@koitt03-A-PC>
2018-07-17 09:24:45 +0900

    2018.07.16

---
+++ test/src/Pratice/Jpw.java
@@ -0,0 +1,32 @@
+package Pratice;
+
+public class Jpw {
+	
+// 1부터 10까지 찍는 소스를 작성 하시오	
+	public static void main(String[] args) {
+		for(int i=0; i<=10; i++) {
+			System.out.println(i);
+		}
+
+
+	}
+}
+//반복문으로 10 부터 1까지 짝수만 찍는 코드를 작성 하시오
+public class Jpw {
+	public static void main(String[] args) {
+	for(int i=10; i>10; i-- )	{
+		if(i%2==0);
+		System.out.println(i);
+	}
+	}
+}
+// 반복문으로 10부터 1까지 홀수만 찍는 코드를 작성하시오
+public class Jpw {
+	public static void main(String[] args) {
+		for(int i=10; i>10; i--) {
+			if(i%2==1);
+			System.out.println(i);
+		}
+	}
+
+}
