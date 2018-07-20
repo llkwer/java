5fdbf44d76794ae9c66fd828c69b003a30a11ce0
koitt03-A <koitt03-A@koitt03-A-PC>
2018-07-20 17:28:51 +0900

    뭘까?

--- test/src/test25/Dao.java
+++ test/src/test25/Dao.java
@@ -1,5 +1,8 @@
 package test25;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Dao {
 	
 	public static ArrayList<HashMap<String,String>> userList = new ArrayList<HashMap<String,String>>();
